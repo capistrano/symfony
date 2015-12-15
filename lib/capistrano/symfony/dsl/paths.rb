4d4a17b48a2509b95a133814f33cea03877efb46
Dmitry Korotovsky <rmpic30@gmail.com>
2015-12-15 13:19:29 +0300

    Added support for SensioDistribution bundle 5

--- lib/capistrano/symfony/dsl/paths.rb
+++ lib/capistrano/symfony/dsl/paths.rb
@@ -24,6 +24,15 @@ module Capistrano
           release_path.join(fetch(:app_config_path))
       end
 
+      def sensio_distribution_bootstrap_path
+          bootstrap_path = release_path.join('vendor/sensio/distribution-bundle/')
+
+          unless fetch(:sensio_distribution_version_5)
+            bootstrap_path += 'Sensio/Bundle/DistributionBundle/'
+          end
+
+          bootstrap_path += 'Resources/bin/build_bootstrap.php'
+      end
     end
   end
 end
